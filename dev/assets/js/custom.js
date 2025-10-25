180d9178de8b73bf2b60b691e4f7f47589dc1d6d
DanilaZot93 <g9nchik@gmail.com>
2025-10-25 10:52:23 +0300

    calculation person

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,43 @@
+const btnBurger = document.querySelector('.hamburger')
+btnBurger.addEventListener('click', ()=>btnBurger.classList.toggle('is-active') )
+const swiperBanner = new Swiper('.swiper-banner', {
+
+    direction:'horizontal',
+    loop: true,
+    spaceBetween: 24,
+    
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    
+    pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+    },
+    
+});
+
+const header = document.querySelector('header')
+const banner = document.querySelector('.swiper-banner')
+const headerHeight = header.offsetHeight
+
+banner.setAttribute('style', '--header-height: ' + headerHeight + 'px')
+
+const inputCheckbox = document.querySelectorAll('input[type="checkbox"]')
+inputCheckbox.forEach(item =>{
+    item.closest('.input-box').classList.add('input-box_checkbox')
+})
+
+
+
+
+const fileInput = document.querySelectorAll('[type="file"]');
+
+fileInput.forEach(item => {
+    item.addEventListener('change', function() {
+        let fileName = item.closest('.input-box').querySelector('.file-name')
+        fileName.innerHTML = item.files[0].name;
+    })
+})
+
