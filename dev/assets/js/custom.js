bd1c7edca9dc65f46379bcf0973faffa0d3a4bc0
DanilaZot93 <g9nchik@gmail.com>
2025-11-13 10:18:58 +0200

    slider 2

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -42,3 +42,47 @@ const fileName = document.querySelector('#file-name')
 fileInput.addEventListener('change', () => {
     fileName.innerHTML = fileInput.files[0].name;
 });
+
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
+
+const swiperAdvantages = new Swiper('#advantages', {
+    spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    roundLengths: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        992: {
+            spaceBetween: 0,
+            slidesPerView: 'auto',
+            slidesPerGroup: advantagesSlideLength,
+            loop: false,
+            simulateTouch: false,
+            pagination: {
+                enabled: false,
+                el: '.swiper-pagination',
+            }
+        }
+    }
+});
+// const swiper = new Swiper('.swiper', {
+//         direction: 'vertical',
+//         loop: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//   // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
